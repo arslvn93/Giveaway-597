c1fe734bde73351065cf39000338a9c51d071c43
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2026-06-11 07:47:19 -0400

    Update Site

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ const config = {
     "primaryColorOverride": "",
     "secondaryColorOverride": "#FFEE58",
     "ghlWebhookUrl": "https://services.leadconnectorhq.com/hooks/bJbBMjcW619cJrLUJ3kY/webhook-trigger/3yNsurfrt9oBUtRBuygb",
-    "facebookPixelId": "1057475448873422",
+    "facebookPixelId": "1695274021897285",
     "turnstileSiteKey": "",
     "salesGeniusAppApi": "",
     "followUpBossEmail": "",
@@ -173,7 +173,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1780411963713_0",
+      "id": "q_auto_1781178439118_0",
       "questionText": "What's your current situation?",
       "options": [
         {
@@ -195,7 +195,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1780411963713_1",
+      "id": "q_auto_1781178439118_1",
       "questionText": "What are you looking to do?",
       "options": [
         {
@@ -221,7 +221,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1780411963713_2",
+      "id": "q_auto_1781178439118_2",
       "questionText": "When are you planning to move?",
       "options": [
         {
